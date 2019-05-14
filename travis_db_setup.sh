4cb413970bef1efabbd5fe53011289553e26b5b6
saypop <tome.m.dj@gmail.com>
2019-05-14 16:42:27 +0100

    Fixing Travis: routes for manage.py

--- travis_db_setup.sh
+++ travis_db_setup.sh
@@ -1,3 +1,3 @@
-python manage.py db init;
-python manage.py db migrate;
-python manage.py db upgrade;
+python3 src/manage.py db init;
+python3 src/manage.py db migrate;
+python3 src/manage.py db upgrade;
