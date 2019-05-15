d556d6ea8999ec91aaa1e82925a933cb324035b0
Masha <mashabudryte@gmail.com>
2019-05-15 11:52:47 +0100

    Encapsulated front_end files, updated .travis.yml for node

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,4 +1,3 @@
-import Blank from './';
 import React from 'react';
 import logo from './logo.svg';
 import 'semantic-ui-css/semantic.min.css';
@@ -7,25 +6,6 @@ import { Card, Icon, Image } from 'semantic-ui-react'
 
 
 function App() {
-  // return
-  // (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // )
   return(
     <Card>
       <Image src='https://timedotcom.files.wordpress.com/2017/10/229-westerlund-21.jpg?w=1520&quality=85&h=1139' wrapped ui={false} />
