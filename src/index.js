62a718aa4ab2cbcd9eb374ba3299f0d60434fd43
saypop <tome.m.dj@gmail.com>
2019-05-20 15:09:07 +0100

    React rooting: Whoop there it is!

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Card, Icon, Image, Container } from 'semantic-ui-react';
-import { Router, Route, IndexRoute, useRouterHistory } from 'react-router';
-import { createHashHistory } from 'history';
+import { Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom'
 import TopicsPage from './pages/TopicsPage.js'
+import SubTopicsPage from './pages/SubTopicsPage.js'
 import './App.css';
 
 
 ReactDOM.render(
-  <TopicsPage/>,
+  <BrowserRouter>
+        <Route exact path="/" component={TopicsPage} />
+        <Route path="/subtopics" component={SubTopicsPage} />
+  </BrowserRouter>,
   document.getElementById('app')
 );
