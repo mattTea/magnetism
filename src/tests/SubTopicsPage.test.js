73bee16e50e744f1ab511e8020566c8812cadff1
Masha <mashabudryte@gmail.com>
2019-05-20 15:15:20 +0100

    TopicPage added

--- src/tests/SubTopicsPage.test.js
+++ src/tests/SubTopicsPage.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { render } from 'react-testing-library';
-import { CardTemplate } from '../templates/CardTemplate.js';
+import { SubTopicsPage } from '../pages/SubTopicsPage.js';
 import 'jest-dom/extend-expect';
 
 test('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<CardTemplate nextSection="/subtopics" dataSource="/topics"/>, div);
+  ReactDOM.render(<SubTopicsPage />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/tests/TopicsPage.test.js
+++ src/tests/TopicsPage.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { render } from 'react-testing-library';
-import { CardTemplate } from '../templates/CardTemplate.js';
+import { TopicsPage } from '../pages/TopicsPage';
 import 'jest-dom/extend-expect';
 
 test('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<CardTemplate nextSection="/subtopics" dataSource="/topics"/>, div);
+  ReactDOM.render(<TopicsPage />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
