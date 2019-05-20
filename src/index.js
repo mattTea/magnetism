699c0fa71c4a9e8aa719c552bd1f2f2e6c3a80af
saypop <tome.m.dj@gmail.com>
2019-05-20 15:26:58 +0100

    Index update

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import { Route } from 'react-router';
 import { BrowserRouter } from 'react-router-dom'
 import TopicsPage from './pages/TopicsPage.js'
-import SubTopicsPage from './pages/SubTopicsPage.js'
+// import SubTopicsPage from './pages/SubTopicsPage.js'
 import './App.css';
 
 
 ReactDOM.render(
   <BrowserRouter>
         <Route exact path="/" component={TopicsPage} />
-        <Route path="/subtopics" component={SubTopicsPage} />
+        <Route path="/subtopics" component={TopicsPage} />
   </BrowserRouter>,
   document.getElementById('app')
 );
