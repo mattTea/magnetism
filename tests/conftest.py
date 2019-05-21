d06aff1a406a7c419a3c7c73fee3aeac8bba09f3
Sam Ixer <samantha.ixer@johnlewis.co.uk>
2019-05-21 10:31:58 +0100

    Work in progress - drying out tests using fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,25 +5,54 @@ sys.path.append('.')
 from app import app, db
 from api.models import *
 
-@pytest.fixture(scope="function", autouse=True)
-def teardown_module():
-  db.session.query(Resource).delete()
-  db.session.query(SubTopic).delete()
-  db.session.query(Topic).delete()
-  db.session.commit()
+@pytest.fixture(scope="function")
+def insert_data():
+    print("I am in the setup fixture")
+    topic = Topic(
+    name="Swimming"
+    )
+    db.session.add(topic)
+    db.session.commit()
 
-# @pytest.fixture(scope="function", autouse=True)
-# def setup_module():
-#   topic = Topic(
-#     name="Swimming"
-#   )
-#   db.session.add(topic)
-#   db.session.commit()
+    subtopic = SubTopic(
+    name="butterfly",
+    topic_id=topic.id,
+    order=1
+    )
+    db.session.add(subtopic)
+    db.session.commit()
 
-#   subtopic = SubTopic(
-#     name="butterfly",
-#     topic_id=topic.id,
-#     order=1
-#   )
-#   db.session.add(subtopic)
-#   db.session.commit()
+    return topic, subtopic
+
+@pytest.fixture(scope="function")
+def insert_ruby_data():
+    topic = Topic(
+      name="Ruby"
+    )
+    db.session.add(topic)
+    db.session.commit()
+
+    subtopic = SubTopic(
+      name="Arrays",
+      topic_id=topic.id,
+      order=1
+    )
+    db.session.add(subtopic)
+    db.session.commit()
+
+    resource = Resource(
+      name="Array theory",
+      content="Arrays start counting at zero. Trying to access an empty array may throw an out of bounds error",
+      subtopic_id = subtopic.id,
+      rating = 1
+    )
+    db.session.add(resource)
+    db.session.commit()
+
+@pytest.fixture(scope="function")
+def clear_database():
+    print("I am in the teardown fixture")
+    db.session.query(Resource).delete()
+    db.session.query(SubTopic).delete()
+    db.session.query(Topic).delete()
+    db.session.commit()
--- tests/test_controller.py
+++ tests/test_controller.py
@@ -1,61 +1,22 @@
 from flask import Flask, json
 from app import *
 
-def test_topics_route():
-  topic = Topic(
-    name="Mathematics"
-  )
-  db.session.add(topic)
-  db.session.commit()
+def test_topics_route(clear_database, insert_data):
 
   with app.test_client() as c:
     resp = c.get('/topics')
     data = json.loads(resp.data)
-    assert data[0]["name"] == "Mathematics"
+    assert data[-1]["name"] == "Swimming"
 
-def test_subtopics_route():
-  topic = Topic(
-    name="Swimming"
-  )
-  db.session.add(topic)
-  db.session.commit()
-
-  subtopic = SubTopic(
-    name="butterfly",
-    topic_id=topic.id,
-    order=1
-  )
-  db.session.add(subtopic)
-  db.session.commit()
+def test_subtopics_route(clear_database, insert_data):
+  print(insert_data.topic)
 
   with app.test_client() as c:
     resp = c.get('/topics/{}/subtopics'.format(topic.id))
     data = json.loads(resp.data)
     assert data[0]["name"] == "butterfly"
 
-def test_resources_route():
-  topic = Topic(
-    name="Ruby"
-  )
-  db.session.add(topic)
-  db.session.commit()
-
-  subtopic = SubTopic(
-    name="Arrays",
-    topic_id=topic.id,
-    order=1
-  )
-  db.session.add(subtopic)
-  db.session.commit()
-
-  resource = Resource(
-    name="Array theory",
-    content="Arrays start counting at zero. Trying to access an empty array may throw an out of bounds error",
-    subtopic_id = subtopic.id,
-    rating = 1
-  )
-  db.session.add(resource)
-  db.session.commit()
+def test_resources_route(clear_database, insert_ruby_data):
 
   with app.test_client() as c:
     resp = c.get('/topics/{topic}/subtopics/{subtopic}/resources'.format(topic=topic.id, subtopic=subtopic.id))
