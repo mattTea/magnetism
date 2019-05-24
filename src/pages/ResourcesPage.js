f5b5589aa0f7f6a0da6ba0bcb9cbd1ce2a959387
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-05-24 10:43:26 +0100

    Adds navbar to redirect within app

--- src/pages/ResourcesPage.js
+++ src/pages/ResourcesPage.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Container, Form, Input, Button } from 'semantic-ui-react';
 import { ResourceCardTemplate } from '../templates/ResourceCardTemplate';
 import { RatingTemplate } from '../templates/RatingTemplate';
+import { ResourcesNavBar } from '../templates/ResourcesNavBar';
 import '../App.css';
 
 export const ResourcesPage = () => {
@@ -10,9 +11,9 @@ export const ResourcesPage = () => {
 
     return (
       <div>
+        <ResourcesNavBar />
         <ResourceCardTemplate />
       </div>
-
     );
   }
 
