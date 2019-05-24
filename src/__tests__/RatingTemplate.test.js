b8aa2e5b609b858a7bd065a71a54d690ea306815
Masha <mashabudryte@gmail.com>
2019-05-24 11:27:39 +0100

    Rating Template coverage up

---
+++ src/__tests__/RatingTemplate.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'jest-dom/extend-expect';
+import { RatingTemplate } from '../templates/RatingTemplate2';
+import { shallow, mount, configure, render } from 'enzyme';
+// import { fireEvent, getByTestId, render} from "react-testing-library";
+import Adapter from 'enzyme-adapter-react-16';
+import { Container, Form, Input, Button, Rating } from 'semantic-ui-react';
+
+configure({ adapter: new Adapter() })
+
+test('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<RatingTemplate />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+test("Upgrading rating to 2", () => {
+  const wrapper = shallow(<RatingTemplate id={1}/>);
+  const instance = wrapper.instance()
+  instance.setScore({rating: "2"})
+  expect(instance.state.score).toEqual("2")
+});
+
+test("returning correct form after rating set", () => {
+  const wrapper = shallow(<RatingTemplate id={1}/>);
+  const instance = wrapper.instance()
+  instance.setScore({rating: "2"})
+});
